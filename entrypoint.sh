24aa9fd5f3774c23a40015a9248d55cab0c668f2
Muhammad Luthfi <muhammadluthfi059@gmail.com>
2021-07-25 06:30:58 +0700

    update variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd /minio
 wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
-export MINIO_ACCESS_KEY="$ACCESS_KEY"
-export MINIO_SECRET_KEY="$SECRET_KEY"
+export MINIO_ROOT_USER="$USER"
+export MINIO_ROOT_PASSWORD="$PASSWORD"
 ./minio server --address :$PORT /mnt/minio/data
